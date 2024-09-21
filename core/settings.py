dffa3d4819109e666f3cb5b8baed7338ee43ac0d
tioluwa <tioluwa.dev@gmail.com>
2024-09-21 20:20:37 +0100

    Update

--- core/settings.py
+++ core/settings.py
@@ -68,14 +68,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # BASE_DIR should be defined in your settings
     }
 }
+
 
 CONN_MAX_AGE = 60
 
