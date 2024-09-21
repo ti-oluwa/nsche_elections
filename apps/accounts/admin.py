e0130940053ce2b4945ee11b6d32b7ab284f5153
tioluwa <tioluwa.dev@gmail.com>
2024-09-21 10:14:10 +0100

    Update

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,4 +1,3 @@
-import re
 from django.contrib import admin
 from django.http import HttpRequest
 
@@ -9,6 +8,12 @@ from .models import UserAccount
 class UserAccountModelAdmin(admin.ModelAdmin):
     """Custom UserAccount model admin."""
 
+    # Adding search functionality
+    search_fields = ['name', 'email', 'account_type']
+
+    # Adding filter functionality
+    list_filter = ['account_type', 'is_staff', 'is_superuser', 'is_active', 'timezone']
+
     def save_model(self, request, obj, form, change):
         # If password is set, then set it using the set_password method
         if "password" in form.changed_data:
@@ -18,3 +23,4 @@ class UserAccountModelAdmin(admin.ModelAdmin):
 
     def has_module_permission(self, request: HttpRequest) -> bool:
         return request.user.is_superuser
+
